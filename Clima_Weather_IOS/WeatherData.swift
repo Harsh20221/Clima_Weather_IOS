4ae76717f9fa9c87e33f3159ff84799ce495bde2
Harsh <harsh20221@gmail.com>
2024-07-30 01:20:10 +0530

    Comments and More Bug Fixes

--- Clima_Weather_IOS/WeatherData.swift
+++ Clima_Weather_IOS/WeatherData.swift
@@ -11,8 +11,13 @@ struct WeatherData : Decodable { //This is how you mention the data type of the 
     
     let name : String
     let main : Main
+    let weather : [Weather] ///!! Make sure the parameter names like name and weather are exactly same as the ones in the Json File
+    ///!!!!!Make this Weather a Array type [] enclose it with close brackets else you will get type mismatch errors 
 }
 struct Main : Decodable {
     let temp : Double  ///? Temp is the sub data type of the main where some properties like temp , pressure etc are stored
-    
+}
+
+struct Weather : Decodable{
+    let description : String
 }
--- Clima_Weather_IOS/WeatherManager.swift
+++ Clima_Weather_IOS/WeatherManager.swift
@@ -17,21 +17,24 @@ class WeatherManager { ///The city name is passed from the view controller
     
     
     func performrequest(urlString:String){ //Here the url will be processed and url session will execute
-        let url=URL(string: urlString)
-        let session = URLSession(configuration: .default)
-        let task =  session.dataTask(with: url!) { data, response, error in ///*Here we are using a closure
-            ///Closures  are  a type of  functions without func keyword  or it's name
-            //This will be responsible for handling the final data recieved from the URL
-            if error != nil{ //If No data is received then we'lll print error message
-                print(error!)
-                return
-            } ///This method defined below will return the data string
-            if let safedata=data{
-                let dataString = String(data:safedata,encoding: .utf8)
-                self.parseJson(weatherData: safedata)
+       if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task =  session.dataTask(with:url) {
+                ( data, response, error) in ///*Here we are using a closure
+                ///Closures  are  a type of  functions without func keyword  or it's name
+                //This will be responsible for handling the final data recieved from the URL
+                if error != nil{ //If No data is received then we'lll print error message
+                    print(error!)
+                    return
+                } ///This method defined below will return the data string
+                if let safedata=data{
+                    self.parseJson(weatherData: safedata)
+                }
             }
+            task.resume()
         }
-        task.resume()}
+    }
+       
         
         
         /////!!!!!!!MAKE SURE THIS ParseJson  FUNCTION IS NOT A PART of above Perforem request function , shouldn't be nested
@@ -39,6 +42,7 @@ class WeatherManager { ///The city name is passed from the view controller
             let decoder = JSONDecoder()
             do {//////!!!VERY IMP TO CHOOSE datatype in the below line Weatherdata.self , (choose the one Weatherdata with the p logo )and in the from: Parameter  we choose this very own function's argument weatherdata mentioned in line 38 as function argument
                 let decodedData = try decoder.decode(WeatherData.self, from:weatherData)
+                print(decodedData.name)
             } catch {
                 print(error);
             }
